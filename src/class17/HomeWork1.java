47b7a671fa6a465f8cf3843115f2efa1abc13f16
rimmaryspek <rimmateltaeva@gmail.com>
2022-10-08 11:21:11 -0400

    Merge remote-tracking branch 'origin/main' was merged. added. added

--- src/class17/HomeWork1.java
+++ src/class17/HomeWork1.java
@@ -5,17 +5,15 @@ public class HomeWork1 {
     // Create a separate Test class where you will execute both of the constructors 1 by 1.
 
     private int num;
-    HomeWork1(){
+    public HomeWork1(){
+        System.out.println("its empty");
+}
+public HomeWork1(String str){
+    System.out.println("str=" + str);
 
 }
-public HomeWork1(int num1){
-        num=num1;
-}
 
-    public static void main(String[] args) {
-        HomeWork1 hw=new HomeWork1();
-        HomeWork1 hw1=new HomeWork1(6);
-    }
+
 
 }
 
---
+++ src/class18/HomeWork1Compactor18.java
@@ -0,0 +1,12 @@
+package class18;
+
+import class17.HomeWork1;
+
+public class HomeWork1Compactor18 {
+    public static void main(String[] args) {
+        new HomeWork1("Java");
+
+
+
+    }
+}
